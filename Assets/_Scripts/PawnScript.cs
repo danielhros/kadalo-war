bfe4a045ba0d7157a02a074176a30ae58b244f83
daniel <daniel.hros.develop@gmail.com>
2021-11-29 15:39:43 +0100

    position reset

--- Assets/_Scripts/PawnScript.cs
+++ Assets/_Scripts/PawnScript.cs
@@ -42,16 +42,16 @@ public class PawnScript : MonoBehaviour
         }
     }
 
-    //void OnMouseEnter()
-    //{
+    void OnMouseEnter()
+    {
 
-    //    //If your mouse hovers over the GameObject with the script attached, output this message
-    //    Debug.Log("Mouse is over GameObject.");
-    //}
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+    }
 
-    //void OnMouseExit()
-    //{
-    //    //The mouse is no longer hovering over the GameObject so output this message each frame
-    //    Debug.Log("Mouse is no longer on GameObject.");
-    //}
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+    }
 }
