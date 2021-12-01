1515c4313453fe866d826083065a94f9e62ec1b4
daniel <daniel.hros.develop@gmail.com>
2021-12-01 14:50:03 +0100

    esc support

--- Assets/_Scripts/MenuManager.cs
+++ Assets/_Scripts/MenuManager.cs
@@ -7,6 +7,9 @@ public class MenuManager : MonoBehaviour
 
     [SerializeField] private GameObject _arrangeFiguresCanvas;
     [SerializeField] private GameObject _fightCanvas;
+    [SerializeField] private GameObject _sureExitCanvas;
+
+    private bool _sureExitActivate = false;
 
     private void Awake() {
         GameManager.OnGameStateChanged += GameManagerOnOnGameStateChanged;
@@ -19,5 +22,16 @@ public class MenuManager : MonoBehaviour
     private void GameManagerOnOnGameStateChanged(GameState state) {
         _arrangeFiguresCanvas.SetActive(state == GameState.FiguresArrange);
         _fightCanvas.SetActive(state == GameState.Fight);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            ToogleExitCanvas();
+        }
+    }
+
+    public void ToogleExitCanvas() {
+        _sureExitActivate = !_sureExitActivate;
+        _sureExitCanvas.SetActive(_sureExitActivate);
     }
 }
